5b6af3736291988c251af9af9834cc38c31e7c45
SynKolbasyn <syn.kolbasyn.06@gmail.com>
2025-05-31 03:14:41 +0300

    refactor: Refactored time picker

--- library/ui/time_picker.py
+++ library/ui/time_picker.py
@@ -11,9 +11,9 @@ from library.ui.menu import SelectionMenu
 class TimePicker(Frame):
     """Time picker frame."""
 
-    def __init__(self, master: Misc, *args: object, **kwargs: object) -> None:
+    def __init__(self, master: Misc) -> None:
         """Initialize time picker frame."""
-        super().__init__(master, *args, **kwargs)
+        super().__init__(master)
 
         datetime_now = datetime.now(UTC)
 
@@ -27,7 +27,7 @@ class TimePicker(Frame):
         self.hour_menu = SelectionMenu(self, range(24))
         self.minute_menu = SelectionMenu(self, range(60))
 
-        self.after(1, self.check_selected_time)
+        self.check_selected_time()
 
         self.year_menu.pack()
         self.month_menu.pack()
@@ -38,25 +38,10 @@ class TimePicker(Frame):
     def check_selected_time(self) -> None:
         """Check that selected time is greater than now UTC time."""
         datetime_now = datetime.now(UTC)
-        datetime_selected = datetime(
-            year=self.year_menu.value,
-            month=self.month_menu.value,
-            day=self.day_menu.value,
-            hour=self.hour_menu.value,
-            minute=self.minute_menu.value,
-            tzinfo=UTC,
-        )
+        datetime_selected = self.value
 
         if datetime_now - datetime_selected >= timedelta(minutes=1):
-            self.year_menu.value = datetime_now.year
-            self.month_menu.value = datetime_now.month
-
-            _, days_in_month = monthrange(datetime_now.year, datetime_now.month)
-            self.day_menu.set_items(range(1, days_in_month + 1))
-            self.day_menu.value = datetime_now.day
-
-            self.hour_menu.value = datetime_now.hour
-            self.minute_menu.value = datetime_now.minute
+            self.value = datetime_now
 
         self.after(1, self.check_selected_time)
 
@@ -65,3 +50,26 @@ class TimePicker(Frame):
         _, days_in_month = monthrange(self.year_menu.value, self.month_menu.value)
         self.day_menu.set_items(range(1, days_in_month + 1))
         self.day_menu.value = min(self.day_menu.value, days_in_month)
+    
+    @property
+    def value(self) -> datetime:
+        return datetime(
+            year=self.year_menu.value,
+            month=self.month_menu.value,
+            day=self.day_menu.value,
+            hour=self.hour_menu.value,
+            minute=self.minute_menu.value,
+            tzinfo=UTC,
+        )
+    
+    @value.setter
+    def value(self, value: datetime) -> None:
+        self.year_menu.value = value.year
+        self.month_menu.value = value.month
+
+        _, days_in_month = monthrange(value.year, value.month)
+        self.day_menu.set_items(range(1, days_in_month + 1))
+        self.day_menu.value = value.day
+
+        self.hour_menu.value = value.hour
+        self.minute_menu.value = value.minute
