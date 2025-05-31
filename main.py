a3def886857dd0af30b47b42f30f3b1d35bf6b2a
SynKolbasyn <syn.kolbasyn.06@gmail.com>
2025-05-31 03:15:53 +0300

    feat: Added new time range picker and added window geometry setting ability

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Main application module."""
 
+from datetime import datetime
 from tkinter import Tk
 
-from library.ui.time_picker import TimePicker
+from library.ui.time_range_picker import RangeTimePicker
 
 
 class App(Tk):
@@ -11,8 +12,23 @@ class App(Tk):
     def __init__(self) -> None:
         """Initialize main application class."""
         super().__init__()
-        self.time_picker = TimePicker(self)
+
+        self.set_window_geometry()
+        
+        self.time_picker = RangeTimePicker(self, "Predict", self.predict)
         self.time_picker.pack()
+    
+    def set_window_geometry(self, width: int = 1280, height: int = 720) -> None:
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+
+        self.geometry(f"{width}x{height}+{x}+{y}")
+    
+    def predict(self, start: datetime, end: datetime) -> None:
+        print(f"Prediction: {start} -> {end}")
 
 
 if __name__ == "__main__":
