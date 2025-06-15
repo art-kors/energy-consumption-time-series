654969cc2905ea52272ff105169a6a27d7982794
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-06-15 22:11:34 +0300

    refactor(ui): Refactored result dialog

--- library/ui/app.py
+++ library/ui/app.py
@@ -4,59 +4,15 @@ from datetime import datetime, time
 
 import pandas as pd
 from PySide6.QtWidgets import (
-    QCheckBox,
     QComboBox,
-    QDialog,
-    QHeaderView,
     QPushButton,
-    QTableView,
-    QVBoxLayout,
     QWidget,
 )
 
 from library.ui.date_selector import DateSelector
 from library.ui.layout import Layout
-from library.ui.plot import Plot
-from library.ui.table import PandasModel
+from library.ui.result import ResultDialog
 from library.ui.time_selector import TimeSelector
-
-
-class CheckboxDialog(QDialog):
-    def __init__(self, parent, data_frame):
-        super().__init__(parent)
-        self.data_frame = data_frame
-        self.setWindowTitle("Выберите варианты")
-        self.checkboxes = []
-        layout = QVBoxLayout()
-
-        self.table = QTableView()
-        self.model = PandasModel(parent=self, data_frame=data_frame)
-        self.table.setModel(self.model)
-        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        # self.table.horizontalHeader().setStretchLastSection(True)
-        layout.addWidget(self.table)
-
-        for label in data_frame.columns.tolist()[:-1]:
-            cb = QCheckBox(label)
-            self.checkboxes.append(cb)
-            layout.addWidget(cb)
-
-        self.btn_ok = QPushButton("Показать выбранные")
-        self.btn_ok.clicked.connect(self.show_checked)
-        layout.addWidget(self.btn_ok)
-
-        self.setLayout(layout)
-
-    def show_checked(self):
-        checked = [cb.text() for cb in self.checkboxes if cb.isChecked()]
-        y_column = self.data_frame.columns.tolist()[-1]
-        for x_column in checked:
-            Plot(
-                parent=self,
-                data_frame=self.data_frame,
-                x_column=x_column,
-                y_column=y_column,
-            ).show()
 
 
 class App(QWidget):
@@ -77,7 +33,14 @@ class App(QWidget):
         self.btn_show = QPushButton("Показать выбор")
         self.btn_show.clicked.connect(self.show_selection)
 
-        self.setLayout(Layout(self.start_date, self.start_time, self.end_date, self.end_time, self.city_combo, self.btn_show))
+        self.setLayout(Layout(
+            self.start_date,
+            self.start_time,
+            self.end_date,
+            self.end_time,
+            self.city_combo,
+            self.btn_show,
+        ))
 
         self.check_datetime_validity()
 
@@ -97,7 +60,9 @@ class App(QWidget):
 
         if start > end:
             self.btn_show.setEnabled(False)
-            self.btn_show.setToolTip("Время начала не может быть больше времени окончания")
+            self.btn_show.setToolTip(
+                "Время начала не может быть больше времени окончания",
+            )
         else:
             self.btn_show.setEnabled(True)
             self.btn_show.setToolTip("")
@@ -109,5 +74,5 @@ class App(QWidget):
             'Occupation': ['Engineer', 'Doctor', 'Artist'],
             'result': [123, 456, 789],
         })
-        CheckboxDialog(self, dataframe).exec()
+        ResultDialog(self, dataframe).exec()
         city = self.city_combo.currentText()
